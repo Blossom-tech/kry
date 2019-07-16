5e60864c07c42c97cb3e8d1551b2eac1e28ed5f6
mohamed-fadl <mhharktat@gmail.com>
2019-07-16 03:41:55 +0200

    Add basic data service to handle database operations
    The data service does:
    - add a new service by url.
    - get all the services in the database.
    - delete service by its url.

---
+++ src/main/java/se/kry/codetest/DataService.java
@@ -0,0 +1,51 @@
+package se.kry.codetest;
+
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+import java.util.List;
+
+class DataService {
+
+	private DBConnector connector;
+
+	 DataService(DBConnector connector) {
+		this.connector = connector;
+	}
+
+	 Future<Void> addService(String service) {
+	 	Future<Void> result = Future.future();
+		 connector.query("INSERT INTO service(url) VALUES(?)", new JsonArray().add(service)).setHandler(done ->{
+			if(done.succeeded())
+				result.complete();
+			else
+				result.fail(done.cause());
+		});
+		 return result;
+	}
+
+	Future<List<JsonObject>> getAllServices() {
+	 	Future<List<JsonObject>> result = Future.future();
+		connector.query("SELECT * FROM service").setHandler(done ->{
+			if(done.succeeded()){
+				result.complete(done.result().getRows());
+			}
+			else
+				result.fail(done.cause());
+		});
+
+		return result;
+	}
+
+	Future<Void> deleteService(String serviceURL) {
+		Future<Void> result = Future.future();
+		connector.query("DELETE FROM service WHERE url = ?", new JsonArray().add(serviceURL)).setHandler(done ->{
+			if(done.succeeded())
+				result.complete();
+			else
+				result.fail(done.cause());
+		});
+		return result;
+	}
+}
